558d4db31940d657044a36835e5be21407535f59
O.Remilekun <remmylknolaotan@gmail.com>
2023-09-14 23:17:53 +0100

    Add Animals jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Greeting from './Greeting.jsx'
+import Animals from './Animals.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting />
+    <Animals />
   </React.StrictMode>,
 )
