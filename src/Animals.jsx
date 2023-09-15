31b36338f22e3ba4370a81001ae7c0a19841768d
O.Remilekun <remmylknolaotan@gmail.com>
2023-09-15 22:06:15 +0100

    Add using if conditions

--- src/Animals.jsx
+++ src/Animals.jsx
@@ -76,18 +76,48 @@
 
 
 // Using the && operator
+// function List(props) {
+//     return (
+//       <ul>
+//         {props.animals.map((animal) => {
+//           return animal.startsWith("L") && <li key={animal}>{animal}</li>;
+//         })}
+//       </ul>
+//     );
+//   }
+  
+//   function App() {
+//     const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  
+//     return (
+//       <div>
+//         <h1>Animals: </h1>
+//         <List animals={animals} />
+//       </div>
+//     );
+//   }
+
+// Using If conditons
 function List(props) {
+    if (!props.animals) {
+      return <div>Loading...</div>;
+    }
+  
+    if (props.animals.length === 0) {
+      return <div>There are no animals in the list!</div>;
+    }
+  
     return (
       <ul>
         {props.animals.map((animal) => {
-          return animal.startsWith("L") && <li key={animal}>{animal}</li>;
+          return <li key={animal}>{animal}</li>;
         })}
       </ul>
     );
   }
   
   function App() {
-    const animals = ["Lion", "Cow", "Snake", "Lizard"];
+    const animals = [];
   
     return (
       <div>
@@ -97,4 +127,4 @@ function List(props) {
     );
   }
 
-  export default App;
+export default App;
